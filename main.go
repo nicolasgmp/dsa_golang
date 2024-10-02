afc0e56b00203857edf94c63c4096df47cf7c90b
magrininicolas <magrininicolas@outlook.com>
2024-10-01 23:58:36 -0300

    main.go changes

--- main.go
+++ main.go
@@ -7,18 +7,15 @@ import (
 
 func main() {
 	ll := ll.NewLinkedList[string]()
+	ll.Unshift("memphis")
+	ll.Unshift("cássio")
 	ll.Push("hi")
 	ll.Push("corinthians")
 	ll.Push("garro")
 	ll.Push("coronado")
-	ll.Unshift("memphis")
-	ll.Unshift("cássio")
+	ll.Set(2, "romero")
 
-	fmt.Println(ll.Pop())
-	fmt.Println(ll.Pop())
-	fmt.Println(ll.Shift())
-
-	fmt.Println("Lista atual")
+	fmt.Println("Lista Atual")
 	curr := ll.Head
 	for curr != nil {
 		fmt.Println(curr.Val)
